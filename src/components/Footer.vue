496ec3e720a322874b41ddb56fce93957748f539
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2025-02-23 22:26:22 +0800

    ✨ Add responsive header and footer components

---
+++ src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <footer class="flex h-20 flex-col items-center gap-2">
+    <a
+      href="#home"
+      class="duration-300 ease-out hover:-translate-y-1 hover:opacity-75"
+      aria-label="Back to Top"
+    >
+      <FontAwesomeIcon :icon="faAngleUp" class="z-0 w-6" />
+    </a>
+
+    <hr class="w-4/5" />
+
+    <p class="text-sm md:text-base">
+      &copy; 2021 - <time :datetime="currentYear">{{ currentYear }}</time> -
+      Made with ❤ by
+      <a
+        class="hover:text-blue-500"
+        href="https://github.com/zhon12345"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        zhon12345
+      </a>
+    </p>
+  </footer>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
+
+const currentYear = computed(() => new Date().getFullYear().toString());
+</script>
